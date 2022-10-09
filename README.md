53bcf56f9fea850c746c9a486cefac288806df08
Jan <janrobinkautz@gmail.com>
2022-10-09 09:18:21 +0200

    minor changes and readme

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-"# watersort-solving-bot" 
+
+# Watersort-Bot
+
+Have you ever stopped and thought to yourself: 
+
+"Wow, playing mobile games is way to exhausting! Why cant I just order someone else to have fun for me?"
+
+
+In this case this script is just perfect for you! Let the script run overnight and crush all of your friends highscores without using your precious brain resources even once!
+
+This python script automatically solves and progresses through the levels of watersort style mobile games on Android.
+
+
+
+## Installation
+
+```bash
+python -m pip install pure-python-adb
+```
+
+To run this script Android Debug Bridge (adb) must be installed and added to path.
+
+Download SDK Platform-Tools for your operating system.
+
+```bash
+adb start-server
+```
+
+Enable USB-Debugging on your Android Device in the Devoloper Settings, connect it per wire to your PC, and press allow when the adb tries to connect.
+
+
+## Usage
+
+You might have to adjust the settings in main.py accourding to your version of watersort.
+
+```python
+#Settings
+#------------------------------------------------------------------------------------------------------------------------
+save_screenshot = False                #Save a screenshot to find out the pixel positions                               | 
+row_positions = [1200, 1900]           #The y position of the middle of the BOTTOM vile content per row in pixels       |
+y_off = 100                            #The y offset between to colors in a vile                                        |
+border_col = (188, 188, 188, 255)      #The border color of a vile                                                      | 
+threshold = 50                         #The darkness threshold at which a color is considered part of the background    |
+colors_per_vile = 4                    #The maximum number of colors per vile                                           |
+pouring_sleep_time = 2.5               #The number of seconds to wait for a move to finish before continuing            |
+next_button_y = 1700                   #The y-location of the next level button                                         |
+loop = True                            #Set to true if you want it to automatically solve multiple levels in a row      |
+#------------------------------------------------------------------------------------------------------------------------
+
+```
+
+Now just open Watersort on your android device and run main.py!
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
--- main.py
+++ main.py
@@ -4,36 +4,52 @@ from PIL import Image
 from io import BytesIO
 from time import sleep
 
+
+#Settings
+#------------------------------------------------------------------------------------------------------------------------
+save_screenshot = True                 #Save a screenshot to find out the pixel positions                               | 
+row_positions = [1200, 1900]           #The y position of the middle of the BOTTOM vile content per row in pixels       |
+y_off = 100                            #The y offset between to colors in a vile                                        |
+border_col = (188, 188, 188, 255)      #The border color of a vile                                                      | 
+threshold = 50                         #The darkness threshold at which a color is considered part of the background    |
+colors_per_vile = 4                    #The maximum number of colors per vile                                           |
+pouring_sleep_time = 2.5               #The number of seconds to wait for a move to finish before continuing            |
+next_button_y = 1700                   #The y-location of the next level button                                         |
+loop = True                            #Set to true if you want it to automatically solve multiple levels in a row      |
+#------------------------------------------------------------------------------------------------------------------------
+
+#TODO speed up pouring by coupling moves and calculating pour time
+#TODO auto detect settings
+#TODO solve invisible dark levels
+
 client = AdbClient(host="127.0.0.1", port=5037)
 device = client.devices()[0]
 
-rows = 2
-row_pos = [1085, 1760]
-y_off = 100
-border_col = (188, 188, 188, 255)
-
-
-colors_per_vile = 4
+width = 0
 level_viles = []
 vile_positions = []
 
 def extract_vile_content():
+    global width 
     screen = Image.open(BytesIO(device.screencap()))
+    width = screen.width
+    if save_screenshot:
+        screen.save("screenshot.png")
     level_viles.clear()
     vile_positions.clear()
     number_of_colors = 0
     colors = {}
-    for row in range(rows):
+    for row in range(len(row_positions)):
         inside_count = 0
         inside = False
         on_border = False
         for x in range(0, screen.width, 4):
-            pixel = screen.getpixel((x,row_pos[row]))
+            pixel = screen.getpixel((x,row_positions[row]))
             if inside_count == 13 and inside:
-                vile_positions[-1] = (x, row_pos[row]-y_off*2)
+                vile_positions[-1] = (x, row_positions[row]-y_off*2)
                 for col in range(colors_per_vile):
-                    pixel = screen.getpixel((x,row_pos[row] - y_off * col))
-                    if (pixel[0]+pixel[1]+pixel[2])/3 <50:
+                    pixel = screen.getpixel((x,row_positions[row] - y_off * col))
+                    if (pixel[0]+pixel[1]+pixel[2])/3 <threshold:
                         break
                     if pixel not in colors:
                         colors[pixel] = number_of_colors
@@ -121,13 +137,24 @@ def solve(viles):
     found_states = set(frozenset(str(viles[i]) for i in range(len(viles))))
     return solve_rec(viles, found_states)
 
-extract_vile_content()
-print(level_viles)
-moves = solve(level_viles)
-print(len(moves), "moves:", moves)
-for move in moves:
-    apply_move(level_viles, move)
-    device.shell('input tap ' + str(vile_positions[move[0]][0]) + ' ' + str(vile_positions[move[0]][1]))
-    sleep(0.1)
-    device.shell('input tap ' + str(vile_positions[move[1]][0]) + ' ' + str(vile_positions[move[1]][1]))
-    sleep(2)
+if __name__ == "__main__":
+    while True:
+        extract_vile_content()
+        print("Detectet viles: ", level_viles)
+        moves = solve(level_viles)
+        if not moves:
+            exit("Could not solve the level, check your settings to see if viles are beeing recognized correctly.")
+        print(len(moves), "moves:", moves)
+        for move in moves:
+            apply_move(level_viles, move)
+            device.shell('input tap ' + str(vile_positions[move[0]][0]) + ' ' + str(vile_positions[move[0]][1]))
+            sleep(0.1)
+            device.shell('input tap ' + str(vile_positions[move[1]][0]) + ' ' + str(vile_positions[move[1]][1]))
+            sleep(pouring_sleep_time)
+        
+        sleep(1)
+        device.shell('input tap ' + str(width / 2) + ' ' + str(next_button_y))
+        if not loop:
+            break
+
+        sleep(2)
